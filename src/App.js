814e2c7b8b75f3f8c70f7ea8a3b907f14805f6f1
Douglas Xiao <douglas.xiaoke@gmail.com>
2023-07-30 16:53:32 -0700

    fully functional

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useQuery } from 'react-query';
+import axios from 'axios';
+import React from 'react';
 
 function App() {
+  const fetchCoffeeImage = React.useCallback(async () => {
+    const res = await axios.get("http://localhost:3001/randomcoffee");
+    return res.data;
+  }, []);
+  
+  const { data, refetch } = useQuery("CurrentApp", fetchCoffeeImage, {
+    manual: true,
+    enabled: false
+  });
+
+  React.useEffect(() => {
+    refetch();
+  }, [refetch]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={data?.file} className="App-logo" alt="logo" />
+        <button className='button-body' onClick={() => refetch()}>
+          Click to show random coffee image
+        </button>
       </header>
     </div>
   );
